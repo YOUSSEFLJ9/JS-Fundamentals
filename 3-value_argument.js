d598fe8015ebdf7ed4b5b59cc2fa7735225d114d
yousseflj9 <youssefmomen@outlook.com>
2025-09-08 15:17:02 +0100

    Initial commit of local project

---
+++ 3-value_argument.js
@@ -0,0 +1,8 @@
+const { argv } = require('node:process');
+
+if (argv.length === 2) {
+  console.log('No argument');
+}else 
+{
+    console.log(argv[2]);
+}
---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+const { argv } = require('node:process');
+
+// if (argv.length < 4) {
+//   return 0;
+// }else 
+{
+    console.log(argv[2] + " is " + argv[3]);
+}
